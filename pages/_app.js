045cdcf84abcf87bb52a20dd344f7423a80cbe6a
Thanigai <thanigai@excrin.com>
2021-05-02 12:49:46 +0530

    footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/main.scss";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Header></Header>
 			<Component {...pageProps} />
+			<Footer />
 		</Layout>
 	);
 }
